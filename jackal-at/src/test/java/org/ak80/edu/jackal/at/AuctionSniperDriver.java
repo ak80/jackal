247c93ded85a31937d16fb9676e2e9b2afcd7368
Alexander Koch <josef.koch@gmail.com>
2017-08-12 23:23:11 +0200

    begin with wishful testing

--- jackal-at/src/test/java/org/ak80/edu/jackal/at/AuctionSniperDriver.java
+++ jackal-at/src/test/java/org/ak80/edu/jackal/at/AuctionSniperDriver.java
@@ -1,6 +1,10 @@
 package org.ak80.edu.jackal.at;
 
 import org.ak80.edu.jackal.Jackal;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 /**
  * Driver to testing jackal-ui
@@ -9,14 +13,23 @@ public class AuctionSniperDriver {
 
   public AuctionSniperDriver(int timeoutInMillis) {
     // TODO implement
+    open("/");
+//    $("#status").shouldHave(text("Hello, Johny!")); // Waits until element gets text
+
+    // TODO
+    //open("/login");
+    //$(By.name("user.name")).setValue("johny");
+    //$("#submit").click();
+    //$(".loading_progress").should(disappear); // Waits until element disappears
+    //$("#username").shouldHave(text("Hello, Johny!")); // Waits until element gets text
+    showsSniperStatus(Jackal.Status.JOINING);
   }
 
   public void showsSniperStatus(Jackal.Status status) {
-    // TODO implement
+    $("#status").shouldHave(text(status.name()));
   }
 
-
   public void dispose() {
-    // TODO implement
+    // TODO implement?
   }
 }
